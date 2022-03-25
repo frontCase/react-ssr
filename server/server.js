985f8fa4c159e010b9f5a846cb5c4de3d014d1b3
F_Shourabadi <faramarz_sh20@yahoo.com>
2022-03-25 15:39:40 +0430

    handle ssr and ssg with express

--- server/server.js
+++ server/server.js
@@ -1,3 +1,36 @@
+// import React from "react";
+// import express from "express";
+// import fs from "fs";
+// import path from "path";
+
+// import ReactDOMServer from "react-dom/server";
+
+// import App from "../src/App";
+
+// const PORT = 3000;
+
+// const app = express();
+
+// app.use("^/$", (req, res, next) => {
+//     fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
+//         if (err) {
+//             console.log(err);
+//             return res.status(500).send("Some error happened");
+//         }
+//         return res.send(
+//             data.replace(
+//                 '<div id="root"></div>',
+//                 `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+//             )
+//         );
+//     });
+// });
+
+// app.use(express.static(path.resolve(__dirname, '..', 'build')))
+
+// app.listen(PORT, () => {
+//     console.log(`App launched on ${PORT}`);
+// });
 import React from "react";
 import express from "express";
 import fs from "fs";
@@ -11,7 +44,8 @@ const PORT = 3000;
 
 const app = express();
 
-app.use("^/$", (req, res, next) => {
+const fetch = (req, res, next) => {
+    console.log('res==>', res)
     fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
         if (err) {
             console.log(err);
@@ -24,7 +58,9 @@ app.use("^/$", (req, res, next) => {
             )
         );
     });
-});
+}
+
+app.get(["/master", "/home", "/test"], fetch);
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')))
 
